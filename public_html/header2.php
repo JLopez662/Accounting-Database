ccd01de3534431dbc0b98d363c4d00f8e1ad900c
Jorge Lopez Gonzalez <jorge.lopez19@upr.edu>
2023-02-12 01:06:25 -0400

    Updating header2.php

--- public_html/header2.php
+++ public_html/header2.php
@@ -1,11 +1,15 @@
+<!--This code includes the "dbh.php" file, which is assumed to contain 
+the database connection details. -->
 <?php include 'dbh.php'; ?>
 
 <head>
   <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="Devcrud">
+    
+    <!-- This sets the title of the document -->
     <title>Cortes Hernandez CPA</title>
-    <!-- font icons -->
+    <!-- This link tag includes the stylesheet for the font icons -->
     <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
     <!-- Bootstrap + Rubic main styles -->
   <link rel="stylesheet" href="assets/css/style.css">
@@ -13,8 +17,12 @@
 
 <body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
     <nav id="scrollspy" class="navbar page-navbar navbar-dark navbar-expand-md fixed-top" data-spy="affix" data-offset-top="20">
-        <div class="container">
+        
+    <!-- This creates a navigation bar with some specified data attributes for navigation -->
+    <div class="container">
             <a class="navbar-brand" href="index.php"><strong class="text-primary">
+              
+             <!-- This creates a brand link for the navigation bar -->
             <img src="CH.png" class="ch-logo"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span></button>
